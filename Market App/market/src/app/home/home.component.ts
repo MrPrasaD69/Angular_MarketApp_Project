a9788bcf8934112026c6d7bead568fd896ac1a62
Prasad Waghmare <prasadwaghmare6@gmail.com>
2022-03-29 10:45:08 +0530

    Market project

---
+++ Market App/market/src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  /** Based on the screen size, switch from standard to one column per row */
+   cards=[];
+   cardsForHandset=[];
+   cardsForWeb=[];
+   isHandset:boolean=false;
+  isHandsetObserver:Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    
+    map(({ matches }) => {
+      if (matches) {
+        return true;
+      }
+      return false;
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit(){
+    this.isHandsetObserver.subscribe(currentObserverValue => {
+    this.isHandset=currentObserverValue;
+    this.loadCards();
+    })
+  }
+
+
+  loadCards(){
+    this.cards=this.isHandset?this.cardsForHandset:this.cardsForWeb;
+  }
+}
